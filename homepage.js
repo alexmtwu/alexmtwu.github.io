02c9ac903531a99a8f34357eb3414f5206b2f821
Alex Wu <alexmtwu@gmail.com>
2017-08-23 01:33:42 -0700

    update overflow

--- homepage.js
+++ homepage.js
@@ -14,7 +14,7 @@ $('#hamburger').on('click',function(){
 	if($(this).attr('data-click-state') == 1) {
 		$(this).attr('data-click-state', 0)
 		$("#dropdown").slideToggle(180);
-		$('html, body').css('overflow', 'auto', 'height', 'auto')
+		$('html, body').css('overflow-y', 'auto', 'height', 'auto')
 		$("#overlay").hide();
 		$("#logo").css('opacity', '1');
 		// $(".nav-bg").css('box-shadow','0px 2px 40px 0px rgba(0,0,0,0.08)')
@@ -22,7 +22,7 @@ $('#hamburger').on('click',function(){
 	} else {
 		$(this).attr('data-click-state', 1)
 		$("#dropdown").slideToggle(180);
-		$('html, body').css('overflow', 'hidden', 'height', '100%')
+		$('html, body').css('overflow-y', 'hidden', 'height', '100%')
 		$("#overlay").show();
 		$("#logo").css('opacity', '0');
 		// $(".nav-bg").css('box-shadow','0px 0px 0px 0px rgba(0,0,0,0)')
