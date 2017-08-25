dd4b5acb032582369a87e995e03a9ae42ca8c0e5
Alex Wu <alexmtwu@gmail.com>
2017-08-24 18:24:39 -0700

    update

--- homepage.js
+++ homepage.js
@@ -9,27 +9,37 @@ $(document).ready(function(){
 
 // dropdown
 $(document).ready(function(){
+
 $('#hamburger').on('click',function(){
 
 	if($(this).attr('data-click-state') == 1) {
 		$(this).attr('data-click-state', 0)
-		$("#dropdown").slideToggle(150);
+    $("#dropdown").css('opacity', '0');
+		$("#dropdown").slideToggle(350);
 		$('html, body').css('overflow-y', 'auto', 'height', 'auto')
 		$("#overlay").hide();
 		$("#logo").css('opacity', '1');
-		// $(".nav-bg").css('box-shadow','0px 2px 40px 0px rgba(0,0,0,0.08)')
 
 	} else {
 		$(this).attr('data-click-state', 1)
-		$("#dropdown").slideToggle(150);
+    $("#dropdown").css('opacity', '1');
+		$("#dropdown").slideToggle(350);
 		$('html, body').css('overflow-y', 'hidden', 'height', '100%')
 		$("#overlay").show();
 		$("#logo").css('opacity', '0');
-		// $(".nav-bg").css('box-shadow','0px 0px 0px 0px rgba(0,0,0,0)')
-}
 
+
+}
+});
+
+$("#overlay").on('click',function() {
+      $('#hamburger').click();
 });
 });
+
+
+
+
 
 
 // tabs
